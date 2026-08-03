fc23c08bf6c04c0462e9926176dbd48b4db071bb
kartiksaini1110 <kartiksaini1110@gmail.com>
2026-08-03 23:34:50 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if(m<n) return false;
+        int[] arr1 = new int[26];
+        for(int i=0; i<n; i++){
+            arr1[s1.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<=m-n; i++){
+            int[] arr2 = new int[26];
+            for(int j=0; j<n; j++){
+                arr2[s2.charAt(i+j) - 'a']++;
+            }
+            if(isMatched(arr1, arr2)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isMatched(int[] arr1, int[] arr2){
+        for(int i=0; i<26; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+}
