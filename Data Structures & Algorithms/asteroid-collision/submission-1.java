472f0334530bb5536070c1626b1c63136619b664
kartiksaini1110 <kartiksaini1110@gmail.com>
2026-08-14 23:50:16 +0530

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int a : asteroids){
+            while(!stack.isEmpty() && a < 0 && stack.peek() > 0 && stack.peek() < -a){
+                stack.pop();
+            }
+            if(!stack.isEmpty() && a < 0 && stack.peek() > 0){
+                if(stack.peek() == -a){
+                    stack.pop();
+                }
+            }else{
+                stack.push(a);
+            }
+        }
+        int[] arr = new int[stack.size()];
+        for(int i=stack.size()-1; i>=0; i--){
+            arr[i] = stack.pop();
+        }
+        return arr;
+    }
+}
