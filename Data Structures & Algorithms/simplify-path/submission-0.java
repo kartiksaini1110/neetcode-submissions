43a8ed90f871a79b4c8d9d26a16a1e1c454bafa0
kartiksaini1110 <kartiksaini1110@gmail.com>
2026-08-21 18:42:21 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for(String i : arr){
+            if(i.equals("..") && !stack.isEmpty()){
+                stack.pop();
+            }else if(!i.equals("") && !i.equals(".") && !i.equals("..")){
+                stack.push(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String i : stack){
+            sb.append("/");
+            sb.append(i);
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
